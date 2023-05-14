24c8ce46b5d0b0e3f60064071c07face8b0de4e6
Przemek Konieczny <przemyslawkoniecznyy@gmail.com>
2023-05-14 15:33:18 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
+import React from "react";
+
+
+
 
 function App() {
+
+
+const [question,setQuestions]=React.useState("")
+
+React.useState(()=>{
+  fetch("https://opentdb.com/api.php?amount=5").then(res=>res.json()).then(
+    (json)=>{
+      setQuestions(json)
+    })
+
+},[])
+
+const[first,setfirst]=React.useState(true)
+
+
+function toggle(){
+  setfirst(false)
+}
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <img className="firstimg" src="blob1.png"/>
+    <div className="wrapper-main">
+      <h1>Quizzical</h1>
+      <p> Some description if needed</p>
+      <button > Start Quiz</button>
     </div>
+   <img src="blob2.png" className="secondimg"/>
+    </div> 
+
+
+
+
   );
 }
 
